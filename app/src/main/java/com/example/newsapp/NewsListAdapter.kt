fb003b98fa33a2a9910634cd88f94e4c92d633bc
Anuj1510 <anujshahi1510@gmail.com>
2023-02-25 22:42:56 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/newsapp/NewsListAdapter.kt
@@ -0,0 +1,52 @@
+package com.example.newsfresh
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.newsapp.R
+
+class NewsListAdapter(private val listener: NewsItemClicked): RecyclerView.Adapter<NewsViewHolder>() {
+
+    private val items: ArrayList<News> = ArrayList()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder { // ye data laa raha hai
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_news, parent, false)
+        val viewHolder = NewsViewHolder(view)
+        view.setOnClickListener{
+            listener.onItemClicked(items[viewHolder.adapterPosition])
+        }
+        return viewHolder
+    }
+
+    override fun getItemCount(): Int { // ye count bata raha hai ki kitna items hai list mai
+        return items.size
+    }
+
+    override fun onBindViewHolder(holder: NewsViewHolder, position: Int) { // ue data ko bind kr raha hai
+        val currentItem = items[position]
+        holder.titleView.text = currentItem.title
+        holder.author.text = currentItem.author
+        Glide.with(holder.itemView.context).load(currentItem.imageUrl).into(holder.image)
+    }
+
+    fun updateNews(updatedNews: ArrayList<News>) {
+        items.clear()
+        items.addAll(updatedNews)
+
+        notifyDataSetChanged()
+    }
+}
+
+class NewsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    val titleView: TextView = itemView.findViewById(R.id.title)
+    val image: ImageView = itemView.findViewById(R.id.image)
+    val author: TextView = itemView.findViewById(R.id.author)
+}
+
+interface NewsItemClicked {
+    fun onItemClicked(item: News)
+}
